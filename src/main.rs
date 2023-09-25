b14a65077fa32ff5bfb048507c2159312620f653
RJ_Infinity <rjinfinity2004@gmail.com>
2023-09-25 12:01:19 +0100

    extractred get_next_value to a functon

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ pub enum CellValue {
 	Formula(Formula),
 	Err(CellErr),
 }
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Cell{
 	value: CellValue,
 	string_backing: Option<String>,
@@ -47,89 +47,95 @@ pub struct Sheet{
 	file_path: String,
 }
 impl Sheet {
-	fn append_cell(cells: &mut Vec<Cell>, tmp_str: &mut String, value: CellValue, curr: &mut Option<CellValue>) {
-		tmp_str.pop();
-		cells.push(Cell {
-			value: value,
-			string_backing: Some(tmp_str.clone()),
-			changed: false,
-		});
-		tmp_str.clear();
-		*curr = None;
+	fn get_next_value(file_path: &str, line_no: usize, line: &str, start: usize) -> Option<Cell>{
+		let mut curr: Option<CellValue> = None;
+		let mut i = start;
+		let mut val_end = false;
+		let mut in_esc = false;
+		while i < line.len() {
+			let chr = line.chars().nth(i).unwrap();
+			match curr {
+				None => {
+					if chr == ',' {
+						return Some(Cell {
+							value: CellValue::Empty,
+							string_backing: Some(line[start..i].to_string()),
+							changed: false,
+						})
+					}else{curr = match chr {
+						' ' | '\t' => None,
+						'"' => Some(CellValue::String(String::new())),
+						'=' => Some(CellValue::Formula(Formula::Litteral(CellValue::Empty.into()))),
+						'-' | '+' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => Some(CellValue::Number(0.0)),
+						c => exit_with_err_at(&file_path, line_no, i, format!("could not parse `{}` as any valid type", c).as_str())
+					}}
+				}
+				Some(ref mut v) => match v {
+					CellValue::String(ref mut s) => {
+						if val_end {
+							if chr == ',' || chr == ')' { return Some(Cell {
+								value: v.clone(),
+								string_backing: Some(line[start..i].to_string()),
+								changed: false,
+							});}
+						} else if in_esc {
+							s.push(match chr {
+								'\\' | '"' => chr,
+								'n' => '\n',
+								't' => '\t',
+								'r' => '\r',
+								_ => exit_with_err_at(&file_path, line_no, i-1, "Unknown escape sequence."), //remove 1 from j to get the slash loc
+							});
+							in_esc = false;
+						} else { match chr {
+							'\\' => {in_esc = true;},
+							'"' => {val_end = true},
+							c => {s.push(c)},
+						}}
+					}
+					CellValue::Number(ref mut n) => {if chr == ',' || chr == ')' {
+						*n = line[start..i].trim().replace("_", "").parse().unwrap();
+						return Some(Cell {
+							value: v.clone(),
+							string_backing: Some(line[start..i].to_string()),
+							changed: false,
+						});
+					} else if !val_end { match chr {
+						'0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '_' => {/* do nothing */},
+						' ' | '\t' => val_end = true,
+						'-' | '+' => exit_with_err_at(&file_path, line_no, i, "Plus and Minus symbols can only be at the start of a number. To calculate a value use a formula."),
+						d => exit_with_err_at(&file_path, line_no, i, format!("`{}` is not a valid digit in what was parsed as a number if you want a string use quotes (`\"`) if you want an formula start it with an equals (`=`).", d).as_str()),
+					}}else if chr != ' ' && chr != '\t' { exit_with_err_at(&file_path, line_no, i, "Only whitespace allowed after the end of a number. If you want a break in the number use an underscore (`_`).")}},
+					CellValue::Formula(ref mut f) => todo!(),
+					_ => unreachable!(),
+				}
+			}
+			i+=1;
+		}
+		if let Some(ref mut v) = curr {
+			match v {
+				CellValue::String(_) => {},
+				CellValue::Number(ref mut n) => { *n = line[start..].trim().replace("_", "").parse().unwrap(); }
+				CellValue::Formula(_f) => todo!(),
+				_ => unreachable!(),
+			}
+			return Some(Cell {
+				value: v.clone(),
+				string_backing: Some(line[start..].to_string()),
+				changed: false,
+			});
+		}
+		return None;
 	}
 	pub fn new(file_path:String, str: String) -> Self {
 		return Sheet { cells: str.replace("\r","").split("\n").enumerate().map(|(i, line)| {
 			let mut rv: Vec<Cell> = Vec::new();
-			let mut curr: Option<CellValue> = None;
-			let mut tmp_str = String::new();
-			let mut val_end = false;
-			let mut in_esc = false;
-			for (j, chr) in line.chars().enumerate() {
-				// print!("===========================\nchr: {:#?}\nrv: {:#?}\ncurr: {:#?}\ntmp_str: {:#?}\nval_end: {:#?}\nin_esc: {:#?}\n",chr,rv,curr,tmp_str,val_end,in_esc);
-				tmp_str.push(chr);
-				match curr {
-					None => {
-						if chr == ',' {
-							Self::append_cell(&mut rv, &mut tmp_str, CellValue::Empty, &mut curr);
-						}else{
-							curr = match chr {
-								' ' | '\t' => None,
-								'"' => Some(CellValue::String(String::new())),
-								'=' => Some(CellValue::Formula(Formula::Litteral(CellValue::Empty.into()))),
-								'-' | '+' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => Some(CellValue::Number(0.0)),
-								c => exit_with_err_at(&file_path, i, j, format!("could not parse `{}` as any valid type", c).as_str())
-							}
-						}
-					}
-					Some(ref mut v) => match v {
-						CellValue::String(ref mut s) => {
-							if val_end { 
-								if chr == ',' {
-									Self::append_cell(&mut rv, &mut tmp_str, v.clone(), &mut curr);
-									val_end = false;
-								}
-							} else if in_esc {
-								s.push(match chr {
-									'\\' | '"' => chr,
-									'n' => '\n',
-									't' => '\t',
-									'r' => '\r',
-									_ => exit_with_err_at(&file_path, i, j-1, "Unknown escape sequence."), //remove 1 from j to get the slash loc
-								});
-								in_esc = false;
-							} else { match chr {
-								'\\' => {in_esc = true;},
-								'"' => {val_end = true},
-								c => {s.push(c)},
-							}}
-						}
-						CellValue::Number(ref mut n) => {if chr == ',' {
-							*n = tmp_str.strip_suffix(",").unwrap().trim().replace("_", "").parse().unwrap();
-							Self::append_cell(&mut rv, &mut tmp_str, v.clone(), &mut curr);
-							val_end = false;
-						} else if !val_end { match chr {
-							'0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '_' => {/* do nothing */},
-							' ' | '\t' => val_end = true,
-							'-' | '+' => exit_with_err_at(&file_path, i, j, "Plus and Minus symbols can only be at the start of a number. To calculate a value use a formula."),
-							d => exit_with_err_at(&file_path, i, j, format!("`{}` is not a valid digit in what was parsed as a number if you want a string use quotes (`\"`) if you want an formula start it with an equals (`=`).", d).as_str()),
-						}}else if chr != ' ' && chr != '\t' { exit_with_err_at(&file_path, i, j, "Only whitespace allowed after the end of a number. If you want a break in the number use an underscore (`_`).")}},
-						CellValue::Formula(_f) => todo!(),
-						_ => unreachable!(),
-					}
-				}
-			}
-			if let Some(ref mut v) = curr {
-				match v {
-					CellValue::String(_) => {},
-					CellValue::Number(ref mut n) => { *n = tmp_str.trim().replace("_", "").parse().unwrap(); }
-					CellValue::Formula(_f) => todo!(),
-					_ => unreachable!(),
-				}
-				rv.push(Cell {
-					value: v.clone(),
-					string_backing: Some(tmp_str),
-					changed: false,
-				});
+			let mut start = 0;
+			let mut cell = Self::get_next_value(&file_path, i, line, start);
+			while let Some(ref c) = cell {
+				rv.push((*c).clone());
+				start += c.string_backing.as_ref().unwrap().len() + 1;
+				cell = Self::get_next_value(&file_path, i, line, start);
 			}
 			return rv;
 		}).collect::<Vec<_>>(), file_path: file_path };
